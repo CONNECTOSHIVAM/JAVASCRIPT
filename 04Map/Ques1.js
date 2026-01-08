8bd1717d814287a18906494fb4d275005ceb08d2
connectoshivam <connecttoshivam1@gmail.com>
2026-01-08 07:32:38 +0530

    Question are based on maps analogy

--- 04Map/Ques1.js
+++ 04Map/Ques1.js
@@ -2,12 +2,27 @@
 //  of event data with latitude and longitude coordinates, find all events that are
 //  within a specified range of latitude and longitude.
 
-function findEventBasedOnRange(events,latitude,longitude){
-  return events.filter((event) => (event.lat >= latitude[0] && event.lat <= latitude[1]&&
-    event.lng >= longitude[0] && event.lng <= longitude[1])
-  )
-}
+// ==== APPROACH 1 using filter:
+// function findEventBasedOnRange(events,latitude,longitude){
+//   return events.filter((event) => (event.lat >= latitude[0] && event.lat <= latitude[1]&&
+//     event.lng >= longitude[0] && event.lng <= longitude[1])
+//   )
+// }
 
+// === APPROACH 2 using the map.
+
+function findEventBasedOnRange(events,latRange,lngRange){
+
+  return events.map((event)=>{
+    if(event.lat >= latRange[0] && event.lat <= latRange[1]&&
+       event.lng >= lngRange[0] && event.lng <= lngRange[1]
+    ){
+      return event
+    }
+    //otherwise return null
+    return null
+  }).filter(Boolean) // remove the null value.
+}
 
 const events = [
   { name: "Concert", city: "New York", lat: 40.730610, lng: -73.935242 },
