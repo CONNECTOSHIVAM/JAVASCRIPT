27d9e5168fea5436140cfc47078a0edf43bd4eb0
connectoshivam <connecttoshivam1@gmail.com>
2026-01-10 09:23:52 +0530

    Questionare based in serch filter analogy

--- 06Array/Ques1.js
+++ 06Array/Ques1.js
@@ -1,13 +1,14 @@
 // Q1.Write a function getSuggestions that takes an array of product names and a 
 // search query, then returns all product names starting with the search query.
 
-function getSuggestions(product,query){
-    return product.filter((item)=>item.toLowerCase().startsWith(query.toLowerCase()))
+
+function getSuggestions(products,searchQuery){
+
+    return products.filter((item)=>item.toLowerCase().startsWith(searchQuery.toLowerCase()))
 }
 
+const products = ["Shirt", "Shoes", "Hat", "Jeans"];
+const searchQuery = "sh"
 
-const product = ["Shirt", "Shoes", "Hat", "Jeans"]
-const query = "sh"
-
-const suggestedItemList = getSuggestions(product,query)
-console.log(suggestedItemList)
+const filteredItemList = getSuggestions(products,searchQuery)
+console.log(filteredItemList)
