131a81cfda676f6a16a88b6c751afec4fee59ead
connectoshivam <connecttoshivam1@gmail.com>
2026-01-11 00:30:40 +0530

    add the filtering functionlaties

--- 06Array/Ques3.js
+++ 06Array/Ques3.js
@@ -1,29 +1,30 @@
 // Q3.Write a function sortProducts that sorts an array of product objects either by 
 // name (alphabetically) or price (ascending or descending).
 
-function sortProducts(products,sortedBy,order){
+function sortProducts(products, sortBy, order) {
 
-    return products.sort((a,b)=>{
-        if(sortedBy === "name")
-        {
-            if(a.name.toLowerCase() < b.name.toLowerCase()) return order === "asc" ? -1 : 1;
-            if(a.name.toLowerCase() < b.name.toLowerCase()) return order === "asc" ? 1 : -1;
-            return 0;
-        }else if(sortedBy === "price"){
+    return products.sort((a, b) => {
+
+        if (sortBy === "name") {
+            if (a.name.toLowerCase() < b.name.toLowerCase()) return order === 'asc' ? -1 : 1;
+            if (a.name.toLowerCase() > b.name.toLowerCase()) return order === 'asc' ? 1 : -1;
+            return 0
+        } else if (sortBy === "price") {
             return order === "asc" ? a.price - b.price : b.price - a.price;
         }
-    });
+    })
 
 }
 
 const products = [
-  { name: "Shirt", price: 500 },
-  { name: "Shoes", price: 800 },
-  { name: "Hat", price: 300 },
-  { name: "Jeans", price: 1000 }
+    { name: "Shirt", price: 500 },
+    { name: "Shoes", price: 800 },
+    { name: "Hat", price: 300 },
+    { name: "Jeans", price: 1000 }
 ];
-const sortedBy = "price"
+
+const sortBy = "name"
 const order = "asc"
 
-const sortedProducts = sortProducts(products,sortedBy,order)
+const sortedProducts = sortProducts(products, sortBy, order)
 console.log(sortedProducts);
