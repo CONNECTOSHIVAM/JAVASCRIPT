3b6a35ed30a6f0dd9df084161e172bd4853a1d51
connectoshivam <connecttoshivam1@gmail.com>
2026-01-12 18:04:53 +0530

    add the value transformations

---
+++ 08map/Ques1.js
@@ -0,0 +1,10 @@
+// Q1.Simple value transformation
+
+function valueTransformation(nums){
+    return nums.map((elem)=>elem*2)
+}
+
+const nums = [1, 2, 3, 4];
+console.log(valueTransformation(nums));
+
+
