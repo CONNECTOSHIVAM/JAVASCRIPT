2bf3c16507e976ff57637a6d4ca5a9557392e449
connectoshivam <connecttoshivam1@gmail.com>
2026-01-12 10:27:41 +0530

    using reduce make a object along with fruits count

---
+++ 07reduce/reduce3.js
@@ -0,0 +1,13 @@
+// Q4.  Frequency Map (counting)
+
+function getFruitsCount(fruits) {
+
+    return fruits.reduce((acc, fruit) => {
+        acc[fruit] = (acc[fruit] || 0) + 1
+        return acc
+    }, {})
+}
+
+const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+const fruitsC = getFruitsCount(fruits)
+console.log(fruitsC);
