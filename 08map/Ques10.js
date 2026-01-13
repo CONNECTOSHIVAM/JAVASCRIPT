72eec168523ca6c50defa66640a04cca56c8889d
connectoshivam <connecttoshivam1@gmail.com>
2026-01-13 23:48:58 +0530

    immutable thinking analogy

--- 08map/Ques10.js
+++ 08map/Ques10.js
@@ -1,27 +1,26 @@
 // Q10. Immutable thinking (core scientist mindset)
 
-function immutableThinking(users){
+function immutableThinkingAnalogy(users){
+    return users.map((user)=>{
 
-   return users.map((user)=>{    
-    let grade;
-    if(user.score >= 70)
-    {
-        grade = "B"
-    }else if(user.score >= 50 && user.score < 70)
-    {
-        grade = "C"
-    }else{
-        grade = "D"
-    }
+        let grade;
+        if(user.score >= 70){
+            grade = "B"
+        }else if(user.score >= 50 && user.score < 70)
+        {
+            grade = "C"
+        }else{
+            grade = "D"
+        }
 
-    return {...users,grade}})
+        return {...user,grade}
+    })
 }
-
-
 
 const users = [
   { name: "A", score: 50 },
   { name: "B", score: 70 }
 ];
 
-console.log(makeMathematical(users));
+console.log(immutableThinkingAnalogy(users));
+
