c8760d57c2e57271e7e84b87a2fea1c89753596c
connectoshivam <connecttoshivam1@gmail.com>
2026-01-14 20:07:08 +0530

    frequency map analogy

--- 07reduce/reduce3.js
+++ 07reduce/reduce3.js
@@ -1,12 +1,12 @@
 // Q4.  Frequency Map (counting)
 
-// function getFruitsCount(fruits){
+function getFruitsCount(fruits){
 
-//     return fruits.reduce((acc,fruit)=>{
-//         acc[fruit] = (acc[fruit] || 0) + 1
-//         return acc 
-//     },{})
-// }
+    return fruits.reduce((acc,fruit)=>{
+        acc[fruit] = (acc[fruit] || 0) + 1
+        return acc 
+    },{})
+}
 
 
 const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
