494fe84e392e5a311229abf1f548d98510236dc6
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 19:01:31 +0530

    add the build index map analogy

--- 07reduce/reduce7.js
+++ 07reduce/reduce7.js
@@ -1,1 +1,17 @@
-// Q8.
+// Q8.Build index map (fast lookup)
+
+
+function buildIndexMap(products){
+    return products.reduce((acc,item)=> {
+        acc[item.id] = {...item}
+        return acc
+    },{})
+}
+
+const products = [
+  { id: 101, name: "Laptop", price: 50000 },
+  { id: 102, name: "Mouse", price: 500 },
+  { id: 103, name: "Keyboard", price: 1500 }
+];
+
+console.log(buildIndexMap(products));
