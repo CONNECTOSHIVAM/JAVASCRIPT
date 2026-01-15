c239505d0e632ba3f16270f04b26952dc1b3270a
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 18:47:07 +0530

    group by property

---
+++ 07reduce/reduce6.js
@@ -0,0 +1,23 @@
+// Q6.Group by property (industry real use)
+
+function groupByProperty(users){
+
+    return users.reduce((acc,item)=>{
+         
+        if(!acc[item.city])
+        {
+            acc[item.city]= []
+        }
+        acc[item.city].push(item.name)
+        return acc 
+    },{})
+}
+
+const users = [
+  { name: "A", city: "Delhi" },
+  { name: "B", city: "Mumbai" },
+  { name: "C", city: "Delhi" },
+  { name: "D", city: "Kolkata" }
+];
+console.log(groupByProperty(users));
+
