736eeebb21c2678d2b19acaf9e44f3a4f0d1e8bd
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 19:27:04 +0530

    scientist statistics analogy

--- 07reduce/reduce8.js
+++ 07reduce/reduce8.js
@@ -4,12 +4,19 @@ function onePassStatistics(marks){
     return marks.reduce((acc,item)=>{
        acc.count += 1
        acc.sum += item
-       acc.avg = acc.sum /acc.count;
        acc.min = Math.min(acc.min,item) 
        acc.max = Math.max(acc.max,item)
-       acc.passed = item>=40
-       acc.failed = item <40
-    },{})
+       
+       if(item>40){
+         acc.passed += 1
+       }else{
+         acc.failed += 1
+       }
+
+       acc.avg = acc.sum /acc.count;
+       return acc
+    },{count: 0, sum: 0, avg: 0,min: Infinity,max: -Infinity,passed: 0,failed: 0})
+       
 }
 
 const marks = [65, 70, 91, 80, 36, 55, 99];
