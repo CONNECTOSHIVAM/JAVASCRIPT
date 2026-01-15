93b7151dfdb277ffdd80197cee57b355f19bce0b
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 06:46:30 +0530

    flatten 2D array

--- 07reduce/reduce5.js
+++ 07reduce/reduce5.js
@@ -1,3 +1,13 @@
 // // Q6. Flatten 2D array
 
+function flattenArray(matrix){
+
+    return matrix.reduce((acc,a)=>{
+         acc.push(...a)
+         return acc
+    },[])
+}
+
+
 const matrix = [[1,2], [3,4,5], [], [6]];
+console.log(flattenArray(matrix));
