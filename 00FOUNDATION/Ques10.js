a075552cea311d86fdde0333eb35d9e4e839619a
connectoshivam <connecttoshivam1@gmail.com>
2026-01-19 10:14:11 +0530

    celsius to fahrenheit converter

---
+++ 00FOUNDATION/Ques10.js
@@ -0,0 +1,11 @@
+// Q10. Convert Celsius to Fahrenheit
+
+function convertCelsiusToFahrenheit(tempInCelsius){
+
+    return (tempInCelsius*9/5)+32
+}
+
+
+
+const tempInCelsius = 30
+console.log(convertCelsiusToFahrenheit(tempInCelsius));
