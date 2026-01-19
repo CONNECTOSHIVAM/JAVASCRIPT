8a02e8b10000ddaf646f1915dac0fa4394f5a8ea
connectoshivam <connecttoshivam1@gmail.com>
2026-01-19 08:30:18 +0530

    print number 1 to N

---
+++ 00FOUNDATION/Ques4.js
@@ -0,0 +1,16 @@
+// Q4.Print numbers from 1 to N
+
+function printValueTillN(N){
+
+    let i = 1;
+    while(i<=10)
+    {
+        console.log(i);
+        i += 1;
+    }
+}
+
+
+const N = 10
+console.log(printValueTillN(N));
+
