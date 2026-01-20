a7bb5bac0f11cbb3232ff695a827025e7b72faf2
connectoshivam <connecttoshivam1@gmail.com>
2026-01-20 07:22:41 +0530

    count the digits in num

--- 00FOUNDATION/Ques6.js
+++ 00FOUNDATION/Ques6.js
@@ -3,17 +3,29 @@
 
 // APPROACH 1:
 
-function countDigitsInNum(digits)
-{
-    digits = Math.abs(digits)
-    const str = String(digits)
-    return str.length
-}
+// function countDigitsInNum(digits)
+// {
+//     digits = Math.abs(digits)
+//     const str = String(digits)
+//     return str.length
+// }
+
 // APPROACH 2:
 
+function countDigitsInNum(digits){
+    digits = Math.abs(digits)
+    count = 0;
+    while(digits>0)
+    {
+        digits = Math.floor(digits/10);
+        count++;
+    }
+    return count
+}
 
 
 
 
-const digits = -25236;
+
+const digits = -2523986;
 console.log(countDigitsInNum(digits));
