6c9959ad345713e058a83886c234a86948ccd2cb
connectoshivam <connecttoshivam1@gmail.com>
2026-01-20 07:16:20 +0530

    count digits in numwith elegent analogy.

--- 00FOUNDATION/Ques6.js
+++ 00FOUNDATION/Ques6.js
@@ -3,27 +3,17 @@
 
 // APPROACH 1:
 
-// function getDigitsInNumber(digits){
-//     digits = Math.abs(digits)
-//     num = String(digits)
-//     return num.length
-// }
-
-// APPROACH 2:
-function getDigitsInNumber(digits)
+function countDigitsInNum(digits)
 {
     digits = Math.abs(digits)
-    let count = 0
-    while(digits > 0)
-        {
-            digits = Math.floor(digits/10)
-            count += 1;
-        } 
-    return count
+    const str = String(digits)
+    return str.length
 }
+// APPROACH 2:
+
 
 
 
 
 const digits = -25236;
-console.log(getDigitsInNumber(digits));
+console.log(countDigitsInNum(digits));
