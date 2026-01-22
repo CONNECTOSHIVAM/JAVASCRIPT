0c065d02a879e1ae54adcb183c80f9cb6d5a1fb2
connectoshivam <connecttoshivam1@gmail.com>
2026-01-22 19:41:55 +0530

    reverse the string.

--- 00FOUNDATION/Ques8.js
+++ 00FOUNDATION/Ques8.js
@@ -1,9 +1,19 @@
 // Q8. Reverse a string
 
-function reverseString(str){
+// function reverseString(str){
 
+//     let newStr = ""
+//     for(let i = str.length-1; i>=0; i--)
+//     {
+//         newStr += str[i]
+//     }
+//     return newStr
+// }
+
+function reverseString(str)
+{
     let newStr = ""
-    for(let i = str.length-1; i>=0; i--)
+    for(let i = str.length-1;i>=0;i--)
     {
         newStr += str[i]
     }
