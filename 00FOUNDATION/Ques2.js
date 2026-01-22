23d89d01742f4ea9c85cc03a1d8b3646fb962ceb
connectoshivam <connecttoshivam1@gmail.com>
2026-01-22 17:58:01 +0530

    find the max value

--- 00FOUNDATION/Ques2.js
+++ 00FOUNDATION/Ques2.js
@@ -1,21 +1,36 @@
 // Q2. Read N numbers and print their maximum
 
+// function findMax(arrN)
+// {
+//     let max = -Infinity
+//     // APPROACH 1:
+//     // for(let i = 1; i<arrN.length; i++)
+//     // {
+//     //     if(arrN[i]>max)
+//     //     {
+//     //         max = arrN[i]
+//     //     }
+//     // }
+//     // APPROACH 2:
+//     let i = 1;
+//     while(i<arrN.length)
+//     {
+//         if(arrN[i] > max)
+//         {
+//             max = arrN[i]
+//         }
+//         i++;
+//     }
+//     return max
+// }
+
 function findMax(arrN)
 {
     let max = -Infinity
-    // APPROACH 1:
-    // for(let i = 1; i<arrN.length; i++)
-    // {
-    //     if(arrN[i]>max)
-    //     {
-    //         max = arrN[i]
-    //     }
-    // }
-    // APPROACH 2:
-    let i = 1;
-    while(i<arrN.length)
+    let i = 0
+    while(i < arrN.length)
     {
-        if(arrN[i] > max)
+        if(arrN[i]>max)
         {
             max = arrN[i]
         }
