5e2cd2d0f7b236b66de66b8870a10321ba0d51a8
connectoshivam <connecttoshivam1@gmail.com>
2026-01-22 19:59:49 +0530

    check the prime

--- 01CONTROL-DECISION-THINKING/Ques1.js
+++ 01CONTROL-DECISION-THINKING/Ques1.js
@@ -1,23 +1,42 @@
 // Q1.Check if a number is prime
 
-function checkPrime(num)
-{
+// function checkPrime(num)
+// {
+//     if(num<=1)
+//     {
+//         return "Number is not a Prime Number.";
+//     }
+//     let isPrime = true
+//     for(let i = 2;i<=Math.sqrt(num);i++)
+//     {
+//         if(num%i==0)
+//         {
+//             isPrime = false
+//             break;
+//         }
+//     }
+//     return isPrime
+// }
+
+// using while loops.
+function checkPrime(num){
+
     if(num<=1)
     {
-        return "Number is not a Prime Number.";
+        console.log(`${num} is not Prime number.`);
     }
-    let isPrime = true
-    for(let i = 2;i<=Math.sqrt(num);i++)
+    let isPrime = true;
+    let i = 2
+    while(i<=Math.sqrt(num))
     {
-        if(num%i==0)
-        {
-            isPrime = false
-            break;
-        }
+          if(num%i==0)
+          {
+            isPrime = false;
+          }
+          i++;
     }
     return isPrime
 }
 
-
-const num = 12
+const num = 13
 console.log(checkPrime(num));
