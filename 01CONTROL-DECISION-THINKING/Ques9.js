6894bca096df15a46e5dd45ed7211a74ac424828
connectoshivam <connecttoshivam1@gmail.com>
2026-01-23 23:12:04 +0530

    find the leap years

--- 01CONTROL-DECISION-THINKING/Ques9.js
+++ 01CONTROL-DECISION-THINKING/Ques9.js
@@ -1,1 +1,14 @@
-// Q9. 
+// Q9. Check leap year
+
+function isLeapYear(year)
+{
+    if(year%4===0 && (year%100!==0 || year%400===0))
+    {
+        console.log(`${year} is Leap year.`);
+    }else{
+        console.log(`${year} is not Leap year.`);
+    }
+}
+
+const year = 1600;
+console.log(isLeapYear(year))
