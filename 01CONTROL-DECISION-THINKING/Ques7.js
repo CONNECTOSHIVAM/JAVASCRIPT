ee9499b1cacc6f9284e4eb067321b710af50a17b
connectoshivam <connecttoshivam1@gmail.com>
2026-01-23 22:41:27 +0530

    find the gcd

--- 01CONTROL-DECISION-THINKING/Ques7.js
+++ 01CONTROL-DECISION-THINKING/Ques7.js
@@ -1,10 +1,14 @@
 // Q7.Find GCD of two numbers
 
-function findTheGcd(a, b) {
-    let result = Math.min(a, b)
-    while (result > 0) {
-        if (a % result === 0 && b % result === 0) {
-            break;
+function findTheGcd(a,b)
+{
+    let result = Math.min(a,b)
+
+    while(result>0)
+    {
+        if(a%result===0 && b%result===0)
+        {
+            break
         }
         result--;
     }
